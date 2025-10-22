c3ad7155a9e64561e4f1d7f834834014f57f37fe
Varinso <121429708+Varinso@users.noreply.github.com>
2025-10-23 01:14:36 +0600

    Update PostRow.java

--- perentalassist_ready/client/src/main/java/com/pa/client/controllers/PostRow.java
+++ perentalassist_ready/client/src/main/java/com/pa/client/controllers/PostRow.java
@@ -8,13 +8,34 @@ public class PostRow {
   private final int id;
   private final StringProperty author = new SimpleStringProperty();
   private final StringProperty content = new SimpleStringProperty();
+  private final StringProperty imageUrl = new SimpleStringProperty();
   private final StringProperty time = new SimpleStringProperty();
 
-  public PostRow(int id, String author, String content, String time) {
-    this.id = id; this.author.set(author); this.content.set(content); this.time.set(time);
+  public PostRow(int id, String author, String content, String time, String imageUrl) {
+    this.id = id;
+    this.author.set(author);
+    this.content.set(content);
+    this.time.set(time);
+    this.imageUrl.set(imageUrl == null ? "" : imageUrl);
   }
-  public int id() { return id; }
-  public StringProperty authorProperty(){ return author; }
-  public StringProperty contentProperty(){ return content; }
-  public StringProperty timeProperty(){ return time; }
+
+  public int id() {
+    return id;
+  }
+
+  public StringProperty authorProperty() {
+    return author;
+  }
+
+  public StringProperty contentProperty() {
+    return content;
+  }
+
+  public StringProperty timeProperty() {
+    return time;
+  }
+
+  public StringProperty imageUrlProperty() {
+    return imageUrl;
+  }
 }
